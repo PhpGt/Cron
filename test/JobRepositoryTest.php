2cdbbac8aa42f5ce336456b5af85a47248f633f5
Greg Bowler <greg.bowler@g105b.com>
2018-11-19 12:38:04 +0000

    Test JobRepository::create

---
+++ test/JobRepositoryTest.php
@@ -0,0 +1,28 @@
+<?php
+namespace Gt\Cron\Test;
+
+use Cron\CronExpression;
+use Gt\Cron\Job;
+use Gt\Cron\JobRepository;
+use PHPUnit\Framework\TestCase;
+
+class JobRepositoryTest extends TestCase {
+	public function testCreate() {
+		$command = uniqid();
+
+		$repository = new JobRepository();
+		$job = $repository->create(
+			$this->mockExpression(),
+			$command
+		);
+
+		self::assertInstanceOf(Job::class, $job);
+		self::assertEquals($command, $job->getCommand());
+	}
+
+	protected function mockExpression():CronExpression {
+		$expression = self::createMock(CronExpression::class);
+		/** @var CronExpression $expression */
+		return $expression;
+	}
+}
