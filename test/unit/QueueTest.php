77974deaa41809768065f82d88c4331770c5bf14
Greg Bowler <greg.bowler@g105b.com>
2018-11-19 13:16:10 +0000

    Test not empty queue after added

--- test/unit/QueueTest.php
+++ test/unit/QueueTest.php
@@ -1,6 +1,7 @@
 <?php
 namespace Gt\Cron\Test;
 
+use Gt\Cron\Job;
 use Gt\Cron\Queue;
 use PHPUnit\Framework\TestCase;
 
@@ -8,5 +9,13 @@ class QueueTest extends TestCase {
 	public function testGetNextJobEmpty() {
 		$queue = new Queue();
 		self::assertNull($queue->getNextJob());
+	}
+
+	public function testNextJobNotEmpty() {
+		/** @var Job $job */
+		$job = self::createMock(Job::class);
+		$queue = new Queue();
+		$queue->add($job);
+		self::assertNotNull($queue->getNextJob());
 	}
 }
