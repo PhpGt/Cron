2b465fb1f6aa3c87634409ded4b1e92af4ca3fab
Greg Bowler <greg.bowler@g105b.com>
2018-11-21 11:43:50 +0000

    Fully test sources

--- test/unit/RunnerFactoryTest.php
+++ test/unit/RunnerFactoryTest.php
@@ -2,6 +2,7 @@
 namespace Gt\Cron\Test;
 
 use Gt\Cron\CrontabNotFoundException;
+use Gt\Cron\Runner;
 use Gt\Cron\RunnerFactory;
 use PHPUnit\Framework\TestCase;
 
@@ -16,5 +17,24 @@ class RunnerFactoryTest extends TestCase {
 		mkdir($dir, 0775, true);
 		self::expectException(CrontabNotFoundException::class);
 		RunnerFactory::createForProject($dir);
+	}
+
+	public function testCreateForProject() {
+		$dir = implode(DIRECTORY_SEPARATOR, [
+			sys_get_temp_dir(),
+			"phpgt",
+			"cron",
+			"example-project-" . uniqid()
+		]);
+		mkdir($dir, 0775, true);
+		touch(implode(DIRECTORY_SEPARATOR, [
+			$dir,
+			"crontab",
+		]));
+		$runner = RunnerFactory::createForProject(
+			$dir
+		);
+
+		self::assertInstanceOf(Runner::class, $runner);
 	}
 }
