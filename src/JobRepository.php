0847b85a97199ba6cca9eec41db9eb8afc0becf4
Greg Bowler <greg.bowler@g105b.com>
2021-02-15 09:56:28 +0000

    Improve type safety

--- src/JobRepository.php
+++ src/JobRepository.php
@@ -4,7 +4,7 @@ namespace Gt\Cron;
 use Cron\CronExpression;
 
 class JobRepository {
-	public function create(CronExpression $expression, $command):Job {
+	public function create(CronExpression $expression, string $command):Job {
 		return new Job($expression, $command);
 	}
 }
